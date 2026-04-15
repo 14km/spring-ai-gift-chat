1149f6a64c05cf77b650f7d54a0c7c3d2e1982a9
con <con@woowahan.com>
2026-04-15 21:43:40 +0900

    init: add project

---
+++ src/main/java/study1/JokeController.java
@@ -0,0 +1,25 @@
+package study1;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JokeController {
+    private final ChatClient client;
+
+    public JokeController(ChatClient.Builder builder) {
+        this.client = builder.build();
+    }
+
+    @GetMapping("/joke")
+    public String joke(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return client
+                .prompt()
+                .system("당신은 시인 입니다.")
+                .user(message)
+                .call()
+                .content();
+    }
+}
