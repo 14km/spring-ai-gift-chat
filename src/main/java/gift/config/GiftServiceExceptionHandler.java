32c4903169e6dbc7a1df646ace3e90793c5a8ea7
con <con@woowahan.com>
2026-04-22 19:45:46 +0900

    feat: GiftServiceExceptionHandler 추가

---
+++ src/main/java/gift/config/GiftServiceExceptionHandler.java
@@ -0,0 +1,21 @@
+package gift.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GiftServiceExceptionHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Map<String, String>> handleIllegalState(IllegalStateException e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("message", e.getMessage()));
+    }
+}
